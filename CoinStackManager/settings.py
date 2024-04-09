e024e821d9618c41af1749f35d4013a9512c9503
crank-gig <crankgig@gmail.com>
2024-04-09 15:41:54 -0400

    mod

--- CoinStackManager/settings.py
+++ CoinStackManager/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'CoinStackManager.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
@@ -139,12 +139,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Define the base directory for static files (not including app-specific static directories)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 # Define the directory where Django will collect static files for deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
